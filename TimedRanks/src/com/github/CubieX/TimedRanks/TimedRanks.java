ffd1830396d6ab3e8c9ab550d7495e296519fe9b
CubieX <shabazza@gmx.ch>
2012-11-03 17:29:31 +0100

    - added some more framework

--- TimedRanks/src/com/github/CubieX/TimedRanks/TimedRanks.java
+++ TimedRanks/src/com/github/CubieX/TimedRanks/TimedRanks.java
@@ -8,16 +8,16 @@ public class TimedRanks extends JavaPlugin
     private TimedRanksConfigHandler cHandler = null;
     private TimedRanksEntityListener eListener = null;
     private TimedRanksCommandHandler comHandler = null;
-    
+
     private TimedRanks plugin;
     private static final Logger log = Logger.getLogger("Minecraft");
     public static String logPrefix = "[TimedRanks] "; // Prefix to go in front of all log entries
-    
+
     @Override
     public void onEnable()
     {     
         this.plugin = this;       
-        
+
         if (!hookToPermissionSystem() ) {
             log.info(String.format("[%s] - Disabled due to no permission system found!", getDescription().getName()));
             getServer().getPluginManager().disablePlugin(this);
@@ -30,7 +30,7 @@ public class TimedRanks extends JavaPlugin
         getCommand("tr").setExecutor(comHandler);
         eListener = new TimedRanksEntityListener(this,log);        
     }
-    
+
     private boolean hookToPermissionSystem() 
     {
         if (getServer().getPluginManager().getPlugin("PermissionsEx") == null)
@@ -42,7 +42,7 @@ public class TimedRanks extends JavaPlugin
             return true;
         }
     }
-
+    //TODO löschen von ganzen keys mit Unterpunkten: plugin.getConfig().getConfigurationSection("Warps").set(args[0], null) + dann config saven!
     @Override
     public void onDisable()
     {       
